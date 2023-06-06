e5a8cb157df1f70c8554ff02d481003387c6318e
nishizawayuya <22aw0128@jec.ac.jp>
2023-06-06 13:51:08 +0900

    クリックしたら回転するように修正

--- App.js
+++ App.js
@@ -1,76 +1,48 @@
-import React from 'react';
-import { View, Text, StyleSheet, Animated, TouchableOpacity, Image } from 'react-native';
+import React, { useRef } from 'react';
+import { StyleSheet, View, Image, TouchableOpacity, Animated } from 'react-native';
 
-/**
- * Animation設定
- * 0〜300フレームの間で増減する値を宣言する
- * CSSでいうところの@keyframeのようなイメージ
- */
-const interpolationConfig = {
-    // 入力される値(フレーム)
-    inputRange: [0,100,200,300,400],
-    // 出力される値
-    outputRange: ['0deg','90deg','180deg','270deg','360deg'],
-}
+export default function App() {
+  const rotationValue = useRef(new Animated.Value(0)).current;
 
-export default class Test extends React.Component {
+  const rotateImage = () => {
+    Animated.timing(rotationValue, {
+      toValue: 1,
+      duration: 2000,
+      useNativeDriver: true,
+    }).start(() => {
+      rotationValue.setValue(0);
+    });
+  };
 
-    /**
-     * constructor
-     * @param props 
-     */
-    constructor(props){
-        super(props);
-        this.state = {
-            // animatedValueを0で初期化
-            animatedValue : new Animated.Value(0),
-        }
-    }
-    
-    /**
-     * onLayout時処理
-     */
-    handleOnLayout = () => {
-        // animatedValueを300まで1.5sかけて変化させる
-        Animated.timing(this.state.animatedValue, {
-            toValue: 400,
-            duration: 2000,
-            delay: 2000,
-            useNativeDriver : false,
-        }).start();
-    }
+  const interpolatedRotateAnimation = rotationValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
 
-    render = () => {
+  const imageStyle = {
+    transform: [{ rotate: interpolatedRotateAnimation }],
+  };
 
-        const { animatedValue } = this.state;
-        const translateY = animatedValue.interpolate(interpolationConfig);
-
-        return(
-          <TouchableOpacity
-            onLongPress={() => {
-              alert('長押しタップ成功！');
-            }}
-            style={styles.container}
-          >
-            <View>
-                <Animated.View  style={{ transform : [{rotate : translateY}] }} onLayout={this.handleOnLayout} >
-                  <Animated.Image style={styles.img} source={require("./assets/fruit_orange.png")}></Animated.Image>
-                </Animated.View>
-            </View>
-
-          </TouchableOpacity>
-        )
-    }
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={rotateImage}>
+        <Animated.Image
+          source={require('./assets/fruit_orange.png')}
+          style={[styles.image, imageStyle]}
+        />
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex : 1, 
-        alignItems : 'center',
-        justifyContent : 'center'
-    },
-    img:{
-      width:100,
-      height:100,
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
 });
